30bb833dfaf76f02d9cc4ff93d51be8ff8492567
X <38390595+gadossey@users.noreply.github.com>
2025-01-05 17:33:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.post('/api/webhook', asyncHandler(async (req, res) => {
                             to: phoneNumber,
                             type: 'template',
                             template: {
-                                name: 'nsem_ghana_mit',
+                                name: 'greetings',
                                 language: { code: 'en' },
 
                             },
